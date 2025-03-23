78887af14b5190220725b3b50e05eae9b4dcd365
parthraj27 <parthraj27@gmail.com>
2025-03-23 12:54:28 +0530

    Removed sensitive information

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express")
 require('dotenv').config()
 const app = express();
 const port =  process.env.port;
-
 app.listen(port,()=>{
     console.log(`production line on port ${port}`)
 })
